cdae34b6118f2120f79a4e04f0951d7f9df340a5
Charlie Kriewall <charlie@kriewall.com>
2020-11-13 12:49:03 -0800

    Create Review endpoint. vid 13.83

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,10 +1,22 @@
 import mongoose from 'mongoose'
 
+/*
+  We ref User in the reviewSchema to associate that field with userSchema._id.
+  User data can be retrieved for a given review with populate()
+
+  Population is the process of automatically replacing the specified 
+  paths in the document with document(s) from other collection(s).
+*/
 const reviewSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
   {
     timestamps: true,
