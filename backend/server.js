e53d96548c24da364987b1504b72a36637020c93
Charlie Kriewall <charlie@kriewall.com>
2020-11-13 09:21:41 -0800

    add Morgan HTTP request logger

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
+import morgan from 'morgan'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -10,30 +11,41 @@ import productRoutes from './routes/productRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
+// access environment variables, connect to Mongo, start Express
 dotenv.config()
-
 connectDB()
-
 const app = express()
 
+// morgan middleware - log HTTP requests to console in development
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// express.json middleware - allow express to pull json from req.body
 app.use(express.json())
 
+// confirm express is online if GET is sent to root
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
+// pass routing to route handlers
 app.use('/api/orders', orderRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/upload', uploadRoutes)
 app.use('/api/users', userRoutes)
 
+// endpoint to return paypal client id
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+// create a static folder to contain uploads
+// this path holds images uploaded by Admin
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+// middleware to handle 404s and errors
 app.use(notFound)
 app.use(errorHandler)
 
